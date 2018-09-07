ac99007ff90c033d8bae4e9903ffe2e5244d4b5f
adhi nugraha <nugraha.adhi124@gmail.com>
2018-09-07 15:28:41 +0700

    Updates

--- frontend/src/components/QuestionForm.jsx
+++ frontend/src/components/QuestionForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import Navbar from './Navbar';
-// import Footer from './Footer';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default class QuestionForm extends Component 
 {
