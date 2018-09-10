60d03bfb680f0210876446672bfc291af1466a93
Asril <irsadi.asril@gmail.com>
2018-09-10 15:47:03 +0700

    adding Sign In Button in Navbar

--- backend/app.js
+++ backend/app.js
@@ -6,16 +6,16 @@ const crypto = require('crypto');
 var koneksi = require('cors');
 var app = express();
 
-const dbs = database.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'sikode',
-    port: '8889',
-    multipleStatements:true
-});
-dbs.connect();
-
+// const dbs = database.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'sikode',
+//     port: '3306',
+//     multipleStatements:true
+// });
+// dbs.connect();
+const dbs = require('./config/dbconfig');
 
 var port = 8003;
 
@@ -26,12 +26,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/tampunganFile', express.static('tampunganFile'));
 
-app.listen(port, () => {
+app.get('/', () => {
+    var sql = `SELECT * FROM users`;
+    dbs.query(sql, (err, result) => {
+        console.log(result)
+    })
+})
+app.listen(port, () => 
+{
     console.log('Server berjalan di port '+port+' ....')
 });
 
-
-app.post('/tambahUser', (req, res) => {
+app.post('/tambahUser', (req, res) => 
+{
     // ambil paramater dari fe, eg: namaproduk, harga, file
     
     var fullname = req.body.fullname;
@@ -39,70 +46,104 @@ app.post('/tambahUser', (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
     
-    
     console.log(fullname);
     console.log(username);
     console.log(email);
     console.log(password);
-    if (req.files){
-           var fungsiFile= req.files.file;
-           var fileName = req.files.file.name;
+    if (req.files)
+    {
+        var fungsiFile= req.files.file;
+        var fileName = req.files.file.name;
     
-           fungsiFile.mv("./profpict/" + fileName,(kaloError) => {
-               if(kaloError){
-                //    console.log(kaloError);
-                   res.send('upload failed');
-               }else {
-                //    res.send('upload sukses');
-                //    var sql = `INSERT INTO users VALUES("${''}", "${fullname}", "${username}", "${email}", "${password}", "${fileName}")`;
-                   var sql = `INSERT INTO users SET fullname= "${fullname}", username="${username}", email= "${email}", password="${password}", image="${fileName}"`;
-                    dbs.query(sql, (kaloError, hasilnya) => {
-                        if(kaloError){
-                            throw kaloError;
-                        }
-                        else {
-                            res.send('1')
-                        }
-                    });
-               }
-           })
-       }
-       else
-       {
-           // beda menggunakan SET dan VALUES, kalau values semua harus diisi. sedang SET hanya yang ditentukan saja yang diisi
+        fungsiFile.mv("./profpict/" + fileName,(kaloError) => 
+        {
+            if(kaloError)
+            {
+            //    console.log(kaloError);
+                res.send('upload failed');
+            }
+            else 
+            {
+            //    res.send('upload sukses');
+            //    var sql = `INSERT INTO users VALUES("${''}", "${fullname}", "${username}", "${email}", "${password}", "${fileName}")`;
+                var sql = `INSERT INTO users SET fullname= "${fullname}", username="${username}", email= "${email}", password="${password}", image="${fileName}"`;
+                dbs.query(sql, (kaloError, hasilnya) => 
+                {
+                    if(kaloError)
+                    {
+                        throw kaloError;
+                    }
+                    else 
+                    {
+                        res.send('1')
+                    }
+                });
+            }
+        })
+    }
+    else
+    {
+        // beda menggunakan SET dan VALUES, kalau values semua harus diisi. sedang SET hanya yang ditentukan saja yang diisi
         // var sql = `INSERT INTO users VALUES("${''}", "${fullname}", "${username}", "${email}", "${password}", "${null}")`;
         var sql = `INSERT INTO users SET fullname= "${fullname}", username="${username}", email= "${email}", password="${password}"`;
-        dbs.query(sql, (kaloError, hasilnya) => {
-            if(kaloError){
+        dbs.query(sql, (kaloError, hasilnya) => 
+        {
+            if(kaloError)
+            {
                 throw kaloError;
             }
-             else {
+            else 
+            {
                 res.end('Data berhasil disimpan')
             }
         });
-       }
-    });
+    }
+});
 
-    app.post('/userlogin', (req, res) => {
-        var sql = `SELECT * FROM users`;
-        dbs.query(sql, (error, result) => {
-            if(error) {
-                throw error;
-            } else {
-                var username = req.body.username;
-                var password = req.body.password;
-                console.log(username)
-                console.log(password)
-    
-                for(var i=0; i < result.length; i++ ){
-                    if(username === result[i].username && password === result[i].password)
-                    {       
-                        res.send(username);
-                        break;
-                    } else if(i === result.length - 1) {
-                        res.send('0');
-                    }
+app.post('/userlogin', (req, res) => 
+{
+    var sql = `SELECT * FROM users`;
+    dbs.query(sql, (error, result) => 
+    {
+        if(error) 
+        {
+            throw error;
+        } 
+        else 
+        {
+            var username = req.body.username;
+            var password = req.body.password;
+            // console.log(username)
+            // console.log(password)
+
+            for(var i=0; i < result.length; i++ )
+            {
+                if(username === result[i].username && password === result[i].password)
+                {       
+                    var userID = result[i].id;
+                    res.send((userID).toString());
+                    break;
+                } 
+                else if(i === result.length - 1) 
+                {
+                    res.send('0');
                 }
             }
-        });
+        }
     });
+});
+
+app.post('/datauser', (req, res) =>
+{
+    var userID  = req.body.userID;
+    console.log(userID)
+
+    var pullData = `SELECT * FROM users WHERE id="${userID}"`;
+    dbs.query(pullData, (err,result) =>
+    {
+        if (err) throw err;
+
+        // console.log(result);
+        res.send(result);
+    })
+});
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,73 +5,85 @@ import Cookies from 'universal-cookie'
 
 const cookies = new Cookies()
 
-
-export default class Login extends Component {
-    state= {
-        statusRedirect: false
-      }
+export default class Login extends Component 
+{
+  state= 
+  {
+    statusRedirect: false
+  }
       
-        fungsiLogin = (e) => {
-          var self = this
-         axios.post(`http://localhost:8002/userlogin`, {
-             username: e.username.value,
-             password: e.password.value
-         }).then((hasil) => {
-             var respon= hasil.data
-             if(respon !== 0) {
-              cookies.set("sessionid", respon, {path: '/'})
-              self.setState({
-                          statusRedirect: true
+  fungsiLogin = (e) => 
+  {
+    var self = this;
+    // console.log(e.username.value);
+    // console.log(e.password.value);
+    axios.post(`http://localhost:8003/userlogin`, 
+    {
+      username: e.username.value,
+      password: e.password.value
+    }).then((hasil) => 
+    {
+      var userID= hasil.data
+      if(userID !== 0) 
+      {
+        cookies.set("sessionid", userID, {path: '/'})
+        self.setState({
+                        statusRedirect: true
                       });
-             }
-             
-         });
-       }
+      }    
+    });
+  }
 
-  render() {
+  render() 
+  {
     if(this.state.statusRedirect) return <Redirect to="/"/>
+    
     return (
-      <div className="col-md-6">
-        <div className="box box-info" >
-  <div className="box-header with-border">
-    <h3 className="box-title">Horizontal Form</h3>
-  </div>
-  {/* /.box-header */}
-  {/* form start */}
-  <form className="form-horizontal">
-    <div className="box-body">
-      <div className="form-group">
-        <label htmlFor="username" className="col-sm-2 control-label">username</label>
-        <div className="col-sm-10">
-          <input type="text" ref="username" className="form-control" id="username" placeholder="username" />
-        </div>
-      </div>
-      <div className="form-group">
-        <label htmlFor="inputPassword3" className="col-sm-2 control-label">Password</label>
-        <div className="col-sm-10">
-          <input type="password" ref="password" className="form-control" id="inputPassword3" placeholder="Password" />
-        </div>
-      </div>
-      <div className="form-group">
-        <div className="col-sm-offset-2 col-sm-10">
-          <div className="checkbox">
-            <label>
-              <input type="checkbox" /> Remember me
-            </label>
-          </div>
-        </div>
-      </div>
-    </div>
-    {/* /.box-body */}
-    <div className="box-footer">
-      <button type="submit" className="btn btn-default">Cancel</button>
-      <button type="button" onClick={() => this.fungsiLogin(this.refs)} className="btn btn-info pull-right" >Sign in</button>
-    </div>
-    {/* /.box-footer */}
-  </form>
-</div>
+            <div className="col-md-6">
+              <div className="box box-info" >
+                <div className="box-header with-border">
+                  <h3 className="box-title">Horizontal Form</h3>
+                </div>
+                {/* /.box-header */}
 
-      </div>
-    )
+                {/* form start */}
+                <form className="form-horizontal">
+                  <div className="box-body">
+                    <div className="form-group">
+                      <label htmlFor="username" className="col-sm-2 control-label">username</label>
+                      <div className="col-sm-10">
+                        <input type="text" ref="username" className="form-control" id="username" placeholder="username" />
+                      </div>
+                    </div>
+                    
+                    <div className="form-group">
+                      <label htmlFor="inputPassword3" className="col-sm-2 control-label">Password</label>
+                      <div className="col-sm-10">
+                        <input type="password" ref="password" className="form-control" id="inputPassword3" placeholder="Password" />
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <div className="col-sm-offset-2 col-sm-10">
+                        <div className="checkbox">
+                          <label>
+                            <input type="checkbox" /> Remember me
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* /.box-body */}
+
+                  <div className="box-footer">
+                    <button type="submit" className="btn btn-default">Cancel</button>
+                    <button type="button" onClick={() => this.fungsiLogin(this.refs)} className="btn btn-info pull-right" >Sign in</button>
+                  </div>
+                  {/* /.box-footer */}
+                </form>
+              </div>
+
+            </div>
+            )
   }
 }
